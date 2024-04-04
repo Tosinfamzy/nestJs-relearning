a341b24835b00b8f6fd2be8bf64b9a800fd2e1de
Tosinfamzy <famurewatosin@gmail.com>
2024-04-04 19:43:44 +0100

    setup initial config for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const options = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('Coffee API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
